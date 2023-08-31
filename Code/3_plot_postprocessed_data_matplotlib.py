c3540c1465cac1c361fcf5d973a40a3d4c546f06
Mate Pentek <mpentek@gmail.com>
2023-08-31 21:52:28 +0200

    properly normalized matching matplotlib

--- Code/3_plot_postprocessed_data_matplotlib.py
+++ Code/3_plot_postprocessed_data_matplotlib.py
@@ -73,133 +73,108 @@ print("\nStarting plotting data")
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
-# # MONTHLY WINDS (airport)
-# fig = plt.figure(1)
-# plt.title("Monthly winds in Munich airport")
-# for idx, range_label in enumerate(["<1","1-3","3-6","6-10",">10"]):
-#     if idx == 0:
-#         plt.bar(df_ranges_a["Months"],df_ranges_a[range_label], label=range_label)
-#         prev_values = df_ranges_a[range_label]
-#     else:
-#         plt.bar(df_ranges_a["Months"],df_ranges_a[range_label], bottom=prev_values, label=range_label)
-#         prev_values = np.add(prev_values,df_ranges_a[range_label])
-# plt.grid()
-# plt.legend()
-# plt.savefig(os.path.join(output_folder,"01_MonthWinds_airp.png"))
-# plt.savefig(os.path.join(output_folder,"01_MonthWinds_airp.pdf"))
+# MONTHLY WINDS (airport)
+fig = plt.figure(1)
+plt.title("Monthly winds in Munich airport")
+for idx, range_label in enumerate(["<1","1-3","3-6","6-10",">10"]):
+    if idx == 0:
+        plt.bar(df_ranges_a["Months"],df_ranges_a[range_label], label=range_label)
+        prev_values = df_ranges_a[range_label]
+    else:
+        plt.bar(df_ranges_a["Months"],df_ranges_a[range_label], bottom=prev_values, label=range_label)
+        prev_values = np.add(prev_values,df_ranges_a[range_label])
+plt.grid()
+plt.legend()
+plt.savefig(os.path.join(output_folder,"01_MonthWinds_airp.png"))
+plt.savefig(os.path.join(output_folder,"01_MonthWinds_airp.pdf"))
 
-# # MONTHLY WINDS (city)
-# fig = plt.figure(2)
-# plt.title("Monthly winds in Munich city")
-# for idx, range_label in enumerate(["<1","1-3","3-6","6-10",">10"]):
-#     if idx == 0:
-#         plt.bar(df_ranges_c["Months"],df_ranges_a[range_label], label=range_label)
-#         prev_values = df_ranges_a[range_label]
-#     else:
-#         plt.bar(df_ranges_c["Months"],df_ranges_a[range_label], bottom=prev_values, label=range_label)
-#         prev_values = np.add(prev_values,df_ranges_a[range_label])
-# plt.grid()
-# plt.legend()
-# plt.savefig(os.path.join(output_folder,"02_MonthWinds_city.png"))
-# plt.savefig(os.path.join(output_folder,"02_MonthWinds_city.pdf"))
+# MONTHLY WINDS (city)
+fig = plt.figure(2)
+plt.title("Monthly winds in Munich city")
+for idx, range_label in enumerate(["<1","1-3","3-6","6-10",">10"]):
+    if idx == 0:
+        plt.bar(df_ranges_c["Months"],df_ranges_a[range_label], label=range_label)
+        prev_values = df_ranges_a[range_label]
+    else:
+        plt.bar(df_ranges_c["Months"],df_ranges_a[range_label], bottom=prev_values, label=range_label)
+        prev_values = np.add(prev_values,df_ranges_a[range_label])
+plt.grid()
+plt.legend()
+plt.savefig(os.path.join(output_folder,"02_MonthWinds_city.png"))
+plt.savefig(os.path.join(output_folder,"02_MonthWinds_city.pdf"))
 
 # Windrose needs: https://python-windrose.github.io/windrose/index.html
 # pip install windrose
+# remains as WIP
 
-# WIP
-# fig, ax = plt.subplots()
-# from windrose import WindroseAxes
+# WIND SPEED DISTRIBUTION (airport)
+fig = plt.figure(9)
+plt.title("Wind intenstiy distribution in Munich airport")
+data = df_airp_m['WindSpeed']
+binwidth = 1
+bins=range(int(min(data)), int(max(data)) + binwidth, binwidth)
+# using weights and fixed bins to properly normalize
+plt.hist(data, density=True, cumulative=False, weights=np.ones_like(data)*100./len(data), bins=bins)
+shape, loc, scale = stats.weibull_min.fit(data)
+x_line = np.linspace(np.min(data), np.max(data), 100)
+y_line = stats.weibull_min.pdf(x_line, shape, loc, scale)
+plt.plot(x_line, y_line, 'r-', label='Weibull distribution')
+plt.grid()
+plt.legend()
+plt.savefig(os.path.join(output_folder,"09_Histogram_airp.png"))
+plt.savefig(os.path.join(output_folder,"09_Histogram_airp.pdf"))
 
-# ax = WindroseAxes.from_ax()
-# ax.bar(wd, ws, normed=True, opening=0.8, edgecolor="white")
-# ax.set_legend()
+# WIND SPEED DISTRIBUTION (city)
+fig = plt.figure(10)
+plt.title("Wind intenstiy distribution in Munich city")
+data = df_city_m['WindSpeed']
+binwidth = 1
+bins=range(int(min(data)), int(max(data)) + binwidth, binwidth)
+# using weights and fixed bins to properly normalize
+plt.hist(data, density=True, cumulative=False, weights=np.ones_like(data)*100./len(data), bins=bins)
+shape, loc, scale = stats.weibull_min.fit(data)
+x_line = np.linspace(np.min(data), np.max(data), 100)
+y_line = stats.weibull_min.pdf(x_line, shape, loc, scale)
+plt.plot(x_line, y_line, 'r-', label='Weibull distribution')
+plt.grid()
+plt.legend()
+plt.savefig(os.path.join(output_folder,"10_Histogram_city.png"))
+plt.savefig(os.path.join(output_folder,"10_Histogram_city.pdf"))
 
-# # WINDROSE (airport)
-# fig3 = px.bar_polar(df_dir_a, r="Frequency", theta="Direction", color="SpeedRange [m/s]", title='Wind direction and intensity in Munich airport', template="plotly_dark", color_discrete_sequence= px.colors.sequential.Plasma_r)
-# fig3.write_html(os.path.join(output_folder,"03_WindRose_airp.html"))
-# fig3.write_image(os.path.join(output_folder,"03_WindRose_airp.png"))
-# fig3.write_image(os.path.join(output_folder,"03_WindRose_airp.svg"))
+# WIND SPEED DISTRIBUTION (airport)
+fig = plt.figure(11)
+plt.title("Gust intensity distribution in Munich airport")
+data = df_airp_g['MaxSpeed']
+binwidth = 1
+bins=range(int(min(data)), int(max(data)) + binwidth, binwidth)
+# using weights and fixed bins to properly normalize
+plt.hist(data, density=True, cumulative=False, weights=np.ones_like(data)*100./len(data), bins=bins)
+shape, loc, scale = stats.weibull_min.fit(data)
+x_line = np.linspace(np.min(data), np.max(data), 100)
+y_line = stats.weibull_min.pdf(x_line, shape, loc, scale)
+plt.plot(x_line, y_line, 'r-', label='Weibull distribution')
+plt.grid()
+plt.legend()
+plt.savefig(os.path.join(output_folder,"11_Histogram_gust_airp.png"))
+plt.savefig(os.path.join(output_folder,"11_Histogram_gust_airp.pdf"))
 
-# # WINDROSE (city)
-# fig4 = px.bar_polar(df_dir_c, r="Frequency", theta="Direction", color="SpeedRange [m/s]", title='Wind direction and intensity in Munich city', template="plotly_dark", color_discrete_sequence= px.colors.sequential.Plasma_r)
-# fig4.write_html(os.path.join(output_folder,"04_WindRose_city.html"))
-# fig4.write_image(os.path.join(output_folder,"04_WindRose_city.png"))
-# fig4.write_image(os.path.join(output_folder,"04_WindRose_city.svg"))
-
-# # PRECISE WINDROSE (airport)
-# fig5 = px.bar_polar(df_dir2_a, r="Frequency", theta="Direction", color="SpeedRange [m/s]", title='Wind direction and intensity in Munich airport', template="plotly_dark", color_discrete_sequence= px.colors.sequential.Plasma_r)
-# fig5.write_html(os.path.join(output_folder,"05_WindRose_precise_airp.html"))
-# fig5.write_image(os.path.join(output_folder,"05_WindRose_precise_airp.png"))
-# fig5.write_image(os.path.join(output_folder,"05_WindRose_precise_airp.svg"))
-
-# # PRECISE WINDROSE (city)
-# fig6 = px.bar_polar(df_dir2_c, r="Frequency", theta="Direction", color="SpeedRange [m/s]", title='Wind direction and intensity in Munich city', template="plotly_dark", color_discrete_sequence= px.colors.sequential.Plasma_r)
-# fig6.write_html(os.path.join(output_folder,"06_WindRose_precise_city.html"))
-# fig6.write_image(os.path.join(output_folder,"06_WindRose_precise_city.png"))
-# fig6.write_image(os.path.join(output_folder,"06_WindRose_precise_city.svg"))
-
-# # GUST vs MEAN (airport)
-# fig = plt.figure(7)
-# plt.title("Mean vs Gust intensity in Munich airport")
-# plt.plot(df_comp_a["Months"],df_comp_a["Mean"], 'b-.', label="Mean")
-# plt.plot(df_comp_a["Months"],df_comp_a["Gust"], 'r--', label="Gust")
-# plt.grid()
-# plt.legend()
-# plt.savefig(os.path.join(output_folder,"07_GustvsMean_airp.png"))
-# plt.savefig(os.path.join(output_folder,"07_GustvsMean_airp.pdf"))
-
-# # GUST vs MEAN (city)
-# fig = plt.figure(8)
-# plt.title("Mean vs Gust intensity in Munich city")
-# plt.plot(df_comp_c["Months"],df_comp_a["Mean"], 'b-.', label="Mean")
-# plt.plot(df_comp_c["Months"],df_comp_a["Gust"], 'r--', label="Gust")
-# plt.grid()
-# plt.legend()
-# plt.savefig(os.path.join(output_folder,"08_GustvsMean_city.png"))
-# plt.savefig(os.path.join(output_folder,"08_GustvsMean_city.pdf"))
+# WIND GUST DISTRIBUTION (city)
+fig = plt.figure(12)
+plt.title("Gust intensity distribution in Munich city")
+data = df_city_g['MaxSpeed']
+binwidth = 1
+bins=range(int(min(data)), int(max(data)) + binwidth, binwidth)
+# using weights and fixed bins to properly normalize
+plt.hist(data, density=True, cumulative=False, weights=np.ones_like(data)*100./len(data), bins=bins)
+shape, loc, scale = stats.weibull_min.fit(data)
+x_line = np.linspace(np.min(data), np.max(data), 100)
+y_line = stats.weibull_min.pdf(x_line, shape, loc, scale)
+plt.plot(x_line, y_line, 'r-', label='Weibull distribution')
+plt.grid()
+plt.legend()
+plt.savefig(os.path.join(output_folder,"11_Histogram_gust_city.png"))
+plt.savefig(os.path.join(output_folder,"11_Histogram_gust_city.pdf"))
 
 plt.show()
-
-# # WIND SPEED DISTRIBUTION (airport)
-# fig9 = px.histogram(df_airp_m, x='WindSpeed', nbins=100, title='Wind intenstiy distribution in Munich airport')
-# fig9.update_layout(bargap = 0.05)
-# fig9.update_xaxes(range=[0,20])
-# shape, loc, scale = stats.weibull_min.fit(df_airp_m['WindSpeed'])     # Weibull distribution fit
-# x_line = np.linspace(np.min(df_airp_m['WindSpeed']), np.max(df_airp_m['WindSpeed']), 100)
-# y_line = stats.weibull_min.pdf(x_line, shape, loc, scale) * 125000
-# df_line = pd.DataFrame({"x": x_line, "y": y_line})
-# fig9.add_scatter(x=df_line["x"], y=df_line["y"], mode="lines", name="Weibull Distribution")
-# fig9.write_html(os.path.join(output_folder,"09_Histogram_airp.html"))
-# fig9.write_image(os.path.join(output_folder,"09_Histogram_airp.png"))
-# fig9.write_image(os.path.join(output_folder,"09_Histogram_airp.svg"))
-
-# # WIND SPEED DISTRIBUTION (city)
-# fig10 = px.histogram(df_city_m, x='WindSpeed', nbins=100, title='Wind intenstiy distribution in Munich city')
-# fig10.update_layout(bargap = 0.05)
-# fig10.update_xaxes(range=[0,20])
-# shape, loc, scale = stats.weibull_min.fit(df_city_m['WindSpeed'])     # Weibull distribution fit
-# x_line = np.linspace(np.min(df_city_m['WindSpeed']), np.max(df_city_m['WindSpeed']), 100)
-# y_line = stats.weibull_min.pdf(x_line, shape, loc, scale) * 40450
-# df_line = pd.DataFrame({"x": x_line, "y": y_line})
-# fig10.add_scatter(x=df_line["x"], y=df_line["y"], mode="lines", name="Weibull Distribution")
-# fig10.write_html(os.path.join(output_folder,"10_Histogram_city.html"))
-# fig10.write_image(os.path.join(output_folder,"10_Histogram_city.png"))
-# fig10.write_image(os.path.join(output_folder,"10_Histogram_city.svg"))
-
-# # WIND GUST DISTRIBUTION (airport)
-# fig11 = px.histogram(df_airp_g, x='MaxSpeed', nbins=200, title='Gust intenstiy distribution in Munich airport')
-# fig11.update_layout(bargap = 0.05)
-# fig11.update_xaxes(range=[0,20])
-# fig11.write_html(os.path.join(output_folder,"11_Histogram_gust_airp.html"))
-# fig11.write_image(os.path.join(output_folder,"11_Histogram_gust_airp.png"))
-# fig11.write_image(os.path.join(output_folder,"11_Histogram_gust_airp.svg"))
-
-# # WIND GUST DISTRIBUTION (city)
-# fig12 = px.histogram(df_city_g, x='MaxSpeed', nbins=200, title='Gust intensity distribution in Munich city')
-# fig12.update_layout(bargap = 0.05)
-# fig12.update_xaxes(range=[0,20])
-# fig12.write_html(os.path.join(output_folder,"12_Histogram_gust_city.html"))
-# fig12.write_image(os.path.join(output_folder,"12_Histogram_gust_city.png"))
-# fig12.write_image(os.path.join(output_folder,"12_Histogram_gust_city.svg"))
 
 print("\nEnding plotting data")
